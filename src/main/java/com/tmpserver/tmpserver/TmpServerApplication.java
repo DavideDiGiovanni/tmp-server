017b795dd352b2f3140f22c845b5dd5abdabcf6b
Davide Di Giovanni <davidedigio@yahoo.it>
2023-02-07 12:46:44 +0100

    Initial commit.

---
+++ src/main/java/com/tmpserver/tmpserver/TmpServerApplication.java
@@ -0,0 +1,13 @@
+package com.tmpserver.tmpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmpserver/tmpserver/TmpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmpserver.tmpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
