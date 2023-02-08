5b6cfe50cddb0f133241167b9ebc411f369d436e
Davide Di Giovanni <davidedigio@yahoo.it>
2023-02-08 15:37:21 +0100

    Updated User with persistence annotations.

--- src/main/java/com/tmpserver/tmpserver/user/User.java
+++ src/main/java/com/tmpserver/tmpserver/user/User.java
@@ -1,13 +1,19 @@
 package com.tmpserver.tmpserver.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table
 @AllArgsConstructor @NoArgsConstructor @Getter @Setter
 public class User {
 
+    @Id
     private String email;
     private String name;
     private String familyName;
