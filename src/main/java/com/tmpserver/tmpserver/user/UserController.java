5f2159fe1a223c96b3d150f4d4d6599d2439e3d8
Davide Di Giovanni <davidedigio@yahoo.it>
2023-02-08 11:06:18 +0100

    Updated ApiExceptionHandler, UserController and UserService with new ApiResponse.

--- src/main/java/com/tmpserver/tmpserver/user/UserController.java
+++ src/main/java/com/tmpserver/tmpserver/user/UserController.java
@@ -6,13 +6,17 @@ import com.tmpserver.tmpserver.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/user")
 public class UserController {
 
+    public static final HttpStatus OK = HttpStatus.OK;
     private final UserService userService;
 
     @Autowired
@@ -22,30 +26,33 @@ public class UserController {
 
     @GetMapping("getusers")
     public ResponseEntity<ApiResponse> getUsers(HttpServletRequest request) {
-        ApiResponse response = userService.getUsers();
-        response.setPath(request.getRequestURI());
+        List<User> userList = userService.getUsers();
+        ApiResponse response = new ApiResponse(OK, request);
+        response.setMessage(userList);
 
-        return new ResponseEntity<>(response, response.getStatus());
+        return new ResponseEntity<>(response, OK);
     }
 
     @PostMapping("signup")
     public ResponseEntity<ApiResponse> signUp(HttpServletRequest request,
                        @Valid @RequestBody SignUpRequest signUpRequest) {
 
-        ApiResponse response = userService.addNewUser(signUpRequest);
-        response.setPath(request.getRequestURI());
+        userService.addNewUser(signUpRequest);
+        ApiResponse response = new ApiResponse(OK, request);
+        response.setMessage("User with " + signUpRequest.getEmail() + " registered.");
 
-        return new ResponseEntity<>(response, response.getStatus());
+        return new ResponseEntity<>(response, OK);
     }
 
     @GetMapping("signin")
     public ResponseEntity<ApiResponse> signIn(HttpServletRequest request,
                      @Valid @RequestBody SignInRequest signInRequest) {
 
-        ApiResponse response = userService.getUser(signInRequest);
-        response.setPath(request.getRequestURI());
+        User user = userService.getUser(signInRequest);
+        ApiResponse response = new ApiResponse(OK, request);
+        response.setMessage(user);
 
-        return new ResponseEntity<>(response, response.getStatus());
+        return new ResponseEntity<>(response, OK);
     }
 
 }
--- src/main/java/com/tmpserver/tmpserver/user/UserService.java
+++ src/main/java/com/tmpserver/tmpserver/user/UserService.java
@@ -3,11 +3,9 @@ package com.tmpserver.tmpserver.user;
 import com.tmpserver.tmpserver.mock.UserRepositoryMocked;
 import com.tmpserver.tmpserver.request.SignInRequest;
 import com.tmpserver.tmpserver.request.SignUpRequest;
-import com.tmpserver.tmpserver.response.ApiResponse;
-import com.tmpserver.tmpserver.response.ApiResponseCorrect;
-import com.tmpserver.tmpserver.response.ApiResponseError;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,18 +17,16 @@ public class UserService {
         userRepository = new UserRepositoryMocked();
     }
 
-    public ApiResponse getUsers() {
-        return new ApiResponseCorrect(userRepository.findAll());
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 
-    public ApiResponse addNewUser(SignUpRequest signUpRequest) {
+    public void addNewUser(SignUpRequest signUpRequest) {
         String email = signUpRequest.getEmail();
         Optional<User> userOptional = userRepository.findById(email);
 
         if(userOptional.isPresent()) {
-            return new ApiResponseError(
-                    new IllegalStateException("User with email " + email + " already exists.")
-            );
+            throw new IllegalStateException("User with email " + email + " already exists.");
         }
 
         User user = new User(
@@ -41,32 +37,22 @@ public class UserService {
         );
 
         userRepository.save(user);
-
-        return new ApiResponseCorrect(
-                "User with email " + email + " saved."
-        );
     }
 
-    public ApiResponse getUser(SignInRequest signInRequest) {
+    public User getUser(SignInRequest signInRequest) {
         String email = signInRequest.getEmail();
         Optional<User> userOptional = userRepository.findById(email);
 
         if(userOptional.isEmpty()) {
-            return new ApiResponseError(
-                    new IllegalStateException("User with email " + email + " does not exist.")
-            );
+                throw new IllegalStateException("User with email " + email + " does not exist.");
         }
 
         User user = userOptional.get();
 
         if(!user.getPassword().equals(signInRequest.getPassword())) {
-            return new ApiResponseError(
-                    new IllegalArgumentException("Wrong password")
-            );
+            throw new IllegalArgumentException("Wrong password");
         }
 
-        return new ApiResponseCorrect(
-                user
-        );
+        return user;
     }
 }
